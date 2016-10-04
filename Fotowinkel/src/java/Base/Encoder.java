a1655a54ef0ad19a42c2f855a1378c21fb8b68dd
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-10-04 12:37:28 +0200

    Fixed the encoder code generation and write a unit test for it

--- Fotowinkel/src/java/Base/Encoder.java
+++ Fotowinkel/src/java/Base/Encoder.java
@@ -5,28 +5,54 @@
  */
 package Base;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Rowan
  */
 public class Encoder
 {
+    private static final Random RANDOMISER = new Random();
 
     /**
      * Generates a code string to distribute
+     *
      * @param amount The amount of serial codes to generate
      * @return A series of serial codes
      */
     public static String[] GenerateCodeStrings(int amount)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
-        
-        //Eg.
-//        String[] ret = new String[amount];
-//        for (int i = 0; i < amount; i++)
-//        {
-//            ret[i] = "555-" + i;
-//        }
-//        return ret;
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] generated = md.digest(GenerateRandomByteArray());
+            String number = Arrays.toString(generated).replaceAll("\\D+","");;
+
+            String[] ret = new String[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                String res = number + "-" + i;
+                ret[i] = res;
+            }
+            return ret;
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(Encoder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    private static byte[] GenerateRandomByteArray()
+    {
+        byte[] b = new byte[RANDOMISER.nextInt(50) + 50];
+        RANDOMISER.nextBytes(b);
+        return b;
     }
 }

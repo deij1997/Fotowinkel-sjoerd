878719a4a373897e9a1d4abe3b2368e58d8df3ae
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-10-18 10:56:32 +0200

    Fixed an image-not-found broken image on the upload page

--- Fotowinkel/src/java/Servlets/ProductsServlet.java
+++ Fotowinkel/src/java/Servlets/ProductsServlet.java
@@ -72,8 +72,8 @@ public class ProductsServlet extends HttpServlet
 
                 /* TODO output your page here. You may use following sample code. */
                 out.println("<div class=\"col-sm-4 col-lg-4 col-md-4\">\n"
-                            + "                        <div class=\"thumbnail\">\n"
-                            + "                            <img src=\"" + imgurl + "\" style=\"width: 99%\" alt=\"\">\n"
+                            + "                        <div class=\"thumbnail\" style=\"max-height: 380px\">\n"
+                            + "                            <img src=\"" + imgurl + "\" style=\"width: 99%\" alt=\"\" onerror=\"this.onerror=null;this.src='Images/notfound.png'\">\n"
                             + "                            <div class=\"caption\">\n"
                             + "                                <h4 class=\"pull-right\">" + price + "</h4>\n"
                             + "                                <h4><a href=\"#\">" + title + "</a>\n"

860cc0fca0f995e79ac3a03ef1817ce8896be04d
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-10-19 13:55:13 +0200

    Upgraded database - Smaller - More efficient - Protected photo-update from SQL injection

--- Fotowinkel/src/java/Base/Database.java
+++ Fotowinkel/src/java/Base/Database.java
@@ -7,6 +7,7 @@ package Base;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,11 +16,13 @@ import java.util.List;
  */
 public class Database
 {
-    private final static String url1 = "jdbc:mysql://web0095.zxcs.nl/u4951p4091_fotowinkel";
-    private final static String user1 = "u4951p4091_prof";
-    private final static String pass1 = "fotos";
-    private final static String driver = "com.mysql.jdbc.Driver";
-    private Connection con;
+    private final static String CONNECTION_URL = "jdbc:mysql://web0095.zxcs.nl/u4951p4091_fotowinkel";
+    private final static String ACCOUNT_NAME = "u4951p4091_prof";
+    private final static String PASSWORD = "fotos";
+    private final static String DRIVER = "com.mysql.jdbc.Driver";
+    static Connection con;
+
+    LowerDatabase dab;
 
     public Connection getCon()
     {
@@ -28,214 +31,104 @@ public class Database
 
     public Database()
     {
-        createConnection();
-    }
-
-    public void createConnection()
-    {
         try
         {
-            Class.forName(driver).newInstance();
-            con = DriverManager.getConnection(url1, user1, pass1);
-
+            createConnection();
         }
         catch (Exception e)
         {
-            e.printStackTrace();
         }
     }
 
-    //Test the connection of the database using a simple query
-    //Return a boolean indicating if the test was succesfull
-    private boolean TestConnection()
+    private void createConnection() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException
     {
-        boolean succes = false;
-        try
+        if (con != null && !con.isClosed())
         {
-            Statement state = con.createStatement();
-            ResultSet result = state.executeQuery("Select * From Test");
-            while (result.next())
-            {
-                System.out.print(result.getInt("lel"));
-            }
-            succes = true;
+            return;
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-
-        }
-        return succes;
-    }
-
-    //Test the connection of the database using the TestConnection method
-    public void Test()
-    {
-        if (TestConnection())
-        {
-            System.out.print("Het werkt!!!!!!11111!!!");
-
-        }
-        else
-        {
-            System.out.print("QQQQQQQQQQQQQQQQQQQQQ, tranen vol gestacked!");
-        }
+        Class.forName(DRIVER).newInstance();
+        con = DriverManager.getConnection(CONNECTION_URL, ACCOUNT_NAME, PASSWORD);
     }
 
     public List<Photo> GetPhotos(String code) throws SQLException
     {
+        dab = new LowerDatabase();
         List<Photo> photos = new ArrayList<Photo>();
-        PreparedStatement state2 = null;
-        ResultSet rs2 = null;
         String query = "Select * From `item` Where klantid Like '" + code + "%'";
-        try
+        ResultSet rs2 = dab.getData(query);
+        while (rs2.next())
         {
-            state2 = con.prepareStatement(query);
-            rs2 = state2.executeQuery();
-            while (rs2.next())
-            {
-                photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
-            }
+            photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            if (rs2 != null)
-            {
-                rs2.close();;
-            }
-            if (state2 != null)
-            {
-                state2.close();
-            }
-            if (con != null)
-            {
-                con.close();
-            }
-        }
-
+        dab.close();
         return photos;
 
     }
 
     public List<Photo> GetPhotosByKlantHashedId(int Klantid) throws SQLException
     {
+        dab = new LowerDatabase();
         List<Photo> photos = new ArrayList<Photo>();
-        PreparedStatement state2 = null;
-        ResultSet rs2 = null;
         String query = "Select * From item Where klanthashedid = " + Klantid + "";
-        try
+        ResultSet rs2 = dab.getData(query);
+        while (rs2.next())
         {
-            state2 = con.prepareStatement(query);
-            rs2 = state2.executeQuery();
-            while (rs2.next())
-            {
-                photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
-            }
+            photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            if (rs2 != null)
-            {
-                rs2.close();;
-            }
-            if (state2 != null)
-            {
-                state2.close();
-            }
-            if (con != null)
-            {
-                con.close();
-            }
-        }
-
+        dab.close();
         return photos;
 
     }
 
     public List<Photo> GetAllPhotos() throws SQLException
     {
+        dab = new LowerDatabase();
         List<Photo> photos = new ArrayList<Photo>();
-        PreparedStatement state2 = null;
-        ResultSet rs2 = null;
         String query = "Select * From `item`";
-        try
+        ResultSet rs2 = dab.getData(query);
+        while (rs2.next())
         {
-            state2 = con.prepareStatement(query);
-            rs2 = state2.executeQuery();
-            while (rs2.next())
-            {
-                photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
-            }
+            photos.add(new Photo(rs2.getDouble("prijs"), rs2.getString("code"), rs2.getString("title"), rs2.getString("description")));
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            if (rs2 != null)
-            {
-                rs2.close();;
-            }
-            if (state2 != null)
-            {
-                state2.close();
-            }
-            if (con != null)
-            {
-                con.close();
-            }
-        }
+        dab.close();
 
         return photos;
     }
 
     public void UpdatePhotos(List<Photo> photos) throws SQLException
     {
-        PreparedStatement state2 = null;
-        ResultSet rs2 = null;
-        try
+        dab = new LowerDatabase();
+        for (Photo p : photos)
         {
-            for (Photo p : photos)
+            //String query = "UPDATE `item` SET `prijs`=" + p.price + ", `title`=" + p.GetTitle() + ",`description`=" + p.GetDescription() + " WHERE `code`=" + p.code;
+            String pquery = "UPDATE `item` SET `prijs`=" + p.price + ", `title`=?,`description`=? WHERE `code`=?";
+            String[] parameters = new String[]
             {
-                String query = "UPDATE `item` SET `prijs`=" + p.price + ", `title`=" + p.GetTitle() + ",`description`=" + p.GetDescription() + " WHERE `code`=" + p.code;
-                state2 = con.prepareStatement(query);
-                rs2 = state2.executeQuery();
-            }
+                p.GetTitle(), p.GetDescription(), p.code
+            };
+            dab.sendQuery(pquery, parameters);
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            if (rs2 != null)
-            {
-                rs2.close();;
-            }
-            if (state2 != null)
-            {
-                state2.close();
-            }
-            if (con != null)
-            {
-                con.close();
-            }
-        }
+        dab.close();
     }
 
     public void UpdatePhoto(Photo photo) throws SQLException
     {
-        List<Photo> photos = new ArrayList<Photo>();
-        photos.add(photo);
+        List<Photo> photos = Arrays.asList(photo);
         UpdatePhotos(photos);
+    }
+
+    public boolean ValidateCredentials(String email, String password) throws SQLException
+    {
+        dab = new LowerDatabase();
+        String query = "Select * From `fotograaf` where email = '" + email + "', wachtwoord = '" + password + "'";
+        dab.sendQuery(query);
+        boolean ret = dab.hasFoundData();
+        dab.close();
+        return ret;
+    }
+
+    public boolean CheckIfCustomerExists(String email)
+    {
+        return false;
     }
 }
---
+++ Fotowinkel/src/java/Base/LowerDatabase.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Base;
+
+import static Base.Database.con;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Rowan
+ */
+public class LowerDatabase
+{
+    private PreparedStatement statement = null;
+    private ResultSet result = null;
+
+    public LowerDatabase()
+    {
+
+    }
+
+    /**
+     * Sends a query to the database
+     * @param query The query to send, does not check for injection
+     * @throws SQLException
+     */
+    public void sendQuery(String query) throws SQLException
+    {
+        statement = con.prepareStatement(query);
+        result = statement.executeQuery();
+    }
+    
+    /**
+     * Sends a query to the database
+     * @param query The query to send
+     * @param parameters The query string parameters
+     * @throws SQLException
+     */
+    public void sendQuery(String query, String[] parameters) throws SQLException
+    {
+        statement = con.prepareStatement(query);
+        int i = 0;
+        for(String s : parameters)
+        {
+            statement.setString(++i, s);
+        }
+        result = statement.executeQuery();
+    }
+    
+    /**
+     * Returns the data given by a query
+     * @param query The query that needs to be sent
+     * @param parameters The query string parameters
+     * @return A resultset containing the data received from the query
+     * @throws SQLException 
+     */
+    public ResultSet getData(String query , String[] parameters) throws SQLException
+    {
+        this.sendQuery(query, parameters);
+        return result;
+    }
+
+    /**
+     * Returns the data given by a query
+     * @param query The query that needs to be sent, does not check for injection
+     * @return A resultset containing the data received from the query
+     * @throws SQLException 
+     */
+    public ResultSet getData(String query) throws SQLException
+    {
+        this.sendQuery(query);
+        return result;
+    }
+
+    /**
+     * Returns the last resultset of data
+     * @return the resultset
+     */
+    public ResultSet getData()
+    {
+        return result;
+    }
+
+    /**
+     * Returns whether the resultset contains data. False if null
+     * @return Whether the set is empty or not.
+     * @throws SQLException 
+     */
+    public boolean hasFoundData() throws SQLException
+    {
+        if (result == null) return false;
+        boolean datafound = false;
+        while (result.next())
+        {
+            datafound = true;
+            break;
+        }
+        result.first();
+        return datafound;
+    }
+
+    /**
+     * Closes the connection to the database
+     * @throws SQLException 
+     */
+    protected void close() throws SQLException
+    {
+        if (result != null && !result.isClosed())
+        {
+            result.close();
+        }
+        if (statement != null && !statement.isClosed())
+        {
+            statement.close();
+        }
+        if (con != null && !con.isClosed())
+        {
+            con.close();
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable
+    {
+        super.finalize();
+    }
+}

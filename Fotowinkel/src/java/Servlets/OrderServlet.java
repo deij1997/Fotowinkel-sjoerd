8575201787d83a869b8e642bf823e6a65980036a
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-11-01 13:45:39 +0100

    XSS Injection fix

--- Fotowinkel/src/java/Servlets/OrderServlet.java
+++ Fotowinkel/src/java/Servlets/OrderServlet.java
@@ -77,7 +77,7 @@ public class OrderServlet extends HttpServlet
                 out.println("<div class=\"col-md-12\">\n"
                             + "                        \n"
                             + "                        <div class=\"thumbnail\">\n"
-                            + "                            <img id=\"myImg\" src='"+ imgurl + "' style=\"width: 15%; max-height: 100%;\" class=\"pull-left\" alt='" + description + "' onerror=\"this.onerror=null;this.src='Images/notfound.png'\">\n"
+                            + "                            <img id=\"myImg\" src='"+ imgurl + "' style=\"width: 15%; max-height: 100%;\" class=\"pull-left\" alt='" + Encoder.HTMLEntityEncode(description) + "' onerror=\"this.onerror=null;this.src='Images/notfound.png'\">\n"
                             + "                            \n"
                             + "                            <div class=\"caption\">\n"
                             + "                                <h4 class=\"pull-right\">" + price + "</h4>\n"

074d472b303b5522dfe0ea41228fd48237ed56f4
TheRealGLH <TheRealGLH@users.noreply.github.com>
2016-12-06 12:20:40 +0100

    javascript colors

--- Fotowinkel/src/java/Base/Enums/ColorType.java
+++ Fotowinkel/src/java/Base/Enums/ColorType.java
@@ -21,4 +21,18 @@ public enum ColorType {
    public String toString() {
        return string;
    }
+   
+   public static ColorType getTypeFromString(String string)
+   {
+
+       //voordat je gaat huilen over if-statements, denk er dan aan dat 
+       //je met JavaEE niet hoger dan Source level 6 kan 
+       //en voor een switch statement in een string moet je Source level hoger dan 7 hebben
+       if(string.equals("NoColor")) return ColorType.NOCOLOR;
+       if(string.equals("BlackWhite")) return ColorType.BLACKWHITE;
+       if(string.equals("Sepia")) return ColorType.SEPIA;
+       if(string.equals("Hex")) return ColorType.HEX;
+       return ColorType.NOCOLOR;
+       
+   }
 }
